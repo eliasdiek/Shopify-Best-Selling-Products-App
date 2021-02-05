fc285181c514039c4499632d8b1d67ecdc21c0a9
Elias Diek <elias@holamicasa.com>
2021-02-05 10:42:44 -0500

    init

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+function Index() {
+    return (
+        <Page>
+            <Layout>
+                <EmptyState
+                  heading="Manage your inventory transfers"
+                  action={{content: 'Select Products'}}
+                  image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+                >
+                    <p>Select Products</p>
+                </EmptyState>
+            </Layout>
+        </Page>
+    )
+}
+
+export default Index;
