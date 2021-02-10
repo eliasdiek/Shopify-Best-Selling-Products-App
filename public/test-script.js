7b306bcda8f94e619c87746c563d796c46be6478
Elias Diek <elias@holamicasa.com>
2021-02-10 08:13:12 -0500

    add delete api

--- public/test-script.js
+++ public/test-script.js
@@ -15,7 +15,7 @@ const makeApp = products => {
                 ${products.map(item => {
                     return `
                         <a href="/products/${item.handle}" style="display: flex; align-items: center; padding: 20px 10px; border-top: 1px solid black;">
-                            <img src=${item.images[0]} />
+                            <img src=${item.images[0].originalSrc} />
                             <p>${item.title}</p>
                         </a>
                     `
