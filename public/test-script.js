cfaef54c783f5206125d40b77addb4de8d8c69ea
Elias Diek <elias@holamicasa.com>
2021-02-10 03:03:22 -0500

    update with polaris

--- public/test-script.js
+++ public/test-script.js
@@ -2,4 +2,13 @@ console.log('[This is coming from script tag API]');
 
 const header = $('header.site-header').parent();
 
-header.prepend('<div style="background-color: red; text-align: center">Hello this is coming from the public folder!</div>');
+const makeheader = data => {
+    header.prepend(`<div style="background-color: red; text-align: center">${data}</div>`);
+}
+
+fetch('https://54977972cbf7.ngrok.io/api/products?shop=yourdev.myshopify.com')
+    .then(res => res.json())
+    .then(data => {
+        makeHeader(data.data);
+    })
+    .catch(error => console.log(error));
