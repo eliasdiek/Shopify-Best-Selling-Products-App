12a1a1fdc6c4fd877576047999ad03df3239418d
Elias Diek <elias@holamicasa.com>
2021-02-10 08:02:34 -0500

    add delete api

--- public/test-script.js
+++ public/test-script.js
@@ -1,14 +1,52 @@
 console.log('[This is coming from script tag API]');
 
-const header = $('header.site-header').parent();
+const body = $('body');
 
-const makeHeader = data => {
-    header.prepend(`<div style="background-color: red; text-align: center">${data}</div>`);
+body.css({
+    'position': 'relative'
+});
+
+const shop = Shopify.shop;
+
+const makeApp = products => {
+    const bestSellerContainer = $(
+            `<div>
+                <h3>Our Best Sellers</h3>
+            </div>`
+        )
+        .css({
+            'position': 'fixed',
+            'background-color': '#ffffff',
+            'border': '1px solid black',
+            'bottom': '88px',
+            'right': '25px',
+            'height': '400px',
+            'width': '350px',
+            'display': 'none'
+        });
+
+    const bestSellerButton = $('<img />').attr('src', 'https://cdn.shopify.com/s/files/1/0325/3174/2765/files/bestseller-button-trans.png?v=1584741923')
+        .css({
+            'position': 'fixed',
+            'width': '150px',
+            'bottom': '20px',
+            'right': '20px',
+            'cursor': 'pointer'
+        });
+    
+    body.append(bestSellerButton);
+    body.append(bestSellerContainer);
+
+    bestSellerButton.click(() => {
+        bestSellerContainer.sliderToggle();
+    });
 }
+
 
 fetch('https://cors-anywhere.herokuapp.com/https://5a2bbde28822.ngrok.io/api/products?shop=yourdev.myshopify.com')
     .then(res => res.json())
     .then(data => {
-        makeHeader(data.data);
+        // makeApp(data.data);
+        console.log('[data]', data);
     })
     .catch(error => console.log(error));
