4d3f7ff80639d0aa2cc0181f60ff8d3f811456f3
Elias Diek <elias@holamicasa.com>
2021-02-10 03:10:52 -0500

    update with polaris

--- public/test-script.js
+++ public/test-script.js
@@ -6,7 +6,7 @@ const makeheader = data => {
     header.prepend(`<div style="background-color: red; text-align: center">${data}</div>`);
 }
 
-fetch('https://54977972cbf7.ngrok.io/api/products?shop=yourdev.myshopify.com')
+fetch('http://cors-anywhere.herokuapp.com/https://54977972cbf7.ngrok.io/api/products?shop=yourdev.myshopify.com')
     .then(res => res.json())
     .then(data => {
         makeHeader(data.data);
